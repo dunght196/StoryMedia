6f8039e94eda21cb44852ec31f13bb380cd8fa7a
dunght <dunght196@gmail.com>
2020-01-10 15:45:57 +0700

    fix ui

--- app/src/main/java/com/example/storymedia/MainActivity.kt
+++ app/src/main/java/com/example/storymedia/MainActivity.kt
@@ -49,8 +49,7 @@ class MainActivity : AppCompatActivity() {
                 ConstraintSet.PARENT_ID,
                 ConstraintSet.RIGHT
             )
-            //cccccc
-            // bbbbbbb
+
             constrainSet.connect(
                 textContent.id,
                 ConstraintSet.LEFT,
